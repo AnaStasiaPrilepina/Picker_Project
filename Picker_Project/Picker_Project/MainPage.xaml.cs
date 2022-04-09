8b5c96185205d0ea0e4766ce9489ec6490553b32
AnaStasia <Ana@DESKTOP-U7R1815>
2022-04-09 17:48:28 +0300

    horo

--- Picker_Project/Picker_Project/MainPage.xaml.cs
+++ Picker_Project/Picker_Project/MainPage.xaml.cs
@@ -11,6 +11,7 @@ namespace Picker_Project
     public partial class MainPage : ContentPage
     {
         Button maa, horo, plaan;
+        Image imgMaa, imgHoro, imgPlaan;
         public MainPage()
         {
             maa = new Button
@@ -18,37 +19,43 @@ namespace Picker_Project
                 Text = "Maakonnad",
                 BackgroundColor = Color.BurlyWood,
                 CornerRadius = 50,
+                FontSize = 20
             };
             maa.Clicked += Button_Clicked;
+            imgMaa = new Image { Source = "maakonnad.png" };
             horo = new Button
             {
                 Text = "Horoskoop",
                 BackgroundColor = Color.LightBlue,
                 CornerRadius = 50,
+                FontSize = 20
             };
             horo.Clicked += Button_Clicked;
+            imgHoro = new Image { Source = "circle.png" };
             plaan = new Button
             {
                 Text = "Ajaplaan",
                 BackgroundColor = Color.LightGreen,
                 CornerRadius = 50,
+                FontSize = 20
             };
             plaan.Clicked += Button_Clicked;
-            //StackLayout st = new StackLayout
-            //{
-            //    Children = { maa, horo, plaan },
-            //};
-            //Content = st;
+            imgPlaan = new Image { Source = "clock.jpg" };
 
-            AbsoluteLayout abs = new AbsoluteLayout
-            { Children = { maa, horo, plaan } };
-            AbsoluteLayout.SetLayoutBounds(maa, new Rectangle(0.5, 0.2, 200, 100));
-            AbsoluteLayout.SetLayoutFlags(maa, AbsoluteLayoutFlags.PositionProportional);
-            AbsoluteLayout.SetLayoutBounds(horo, new Rectangle(0.5, 0.5, 200, 100));
-            AbsoluteLayout.SetLayoutFlags(horo, AbsoluteLayoutFlags.PositionProportional);
-            AbsoluteLayout.SetLayoutBounds(plaan, new Rectangle(0.5, 0.8, 200, 100));
-            AbsoluteLayout.SetLayoutFlags(plaan, AbsoluteLayoutFlags.PositionProportional);
-            Content = abs;
+            Grid grid = new Grid
+            {
+                RowDefinitions = { new RowDefinition(), new RowDefinition(), new RowDefinition() },
+                ColumnDefinitions = { new ColumnDefinition(), new ColumnDefinition() },
+            };
+            grid.Children.Add(maa, 0, 0);
+            //grid.Children.Add(horo, 0, 1);
+            grid.Children.Add(imgHoro, 0, 1);
+            grid.Children.Add(plaan, 0, 2);
+            grid.Children.Add(imgMaa, 1, 0);
+            //grid.Children.Add(imgHoro, 1, 1);
+            grid.Children.Add(horo,1, 1);
+            grid.Children.Add(imgPlaan, 1, 2);
+            Content = grid;
         }
 
         private async void Button_Clicked(object sender, EventArgs e)
--- Picker_Project/Picker_Project/horoskop.xaml.cs
+++ Picker_Project/Picker_Project/horoskop.xaml.cs
@@ -12,9 +12,45 @@ namespace Picker_Project
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class horoskop : ContentPage
     {
+        DatePicker dp;
+        Image img;
+        Entry ent;
+        //Editor editor;
+        Label lbl;
         public horoskop()
         {
-            InitializeComponent();
+            dp = new DatePicker
+            {
+                Format = "D",
+                Date = DateTime.Now,
+            };
+            dp.DateSelected += Dp_DateSelected;
+            ent = new Entry
+            {
+                Placeholder = "Znak zodiaka"
+            };
+            ent.Completed += Ent_Completed;
+            img = new Image
+            {
+                Source = "circle.png"
+            };
+
+            lbl = new Label
+            {
+                Text = "Opisanie"
+            };
+            StackLayout st = new StackLayout { Children = { dp, ent, img, lbl } };
+            Content = st;
+        }
+
+        private void Ent_Completed(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Dp_DateSelected(object sender, DateChangedEventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
